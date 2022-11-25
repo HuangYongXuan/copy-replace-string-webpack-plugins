0d755e4d1c48245397c95550443f07b768429fd8
hyx <huangyx@singcheer.com>
2022-11-25 14:58:23 +0800

    feat:更新为最新的webpack

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 /**
- * @module      12
+ * @module      copy-modify-webpack-plugin
  * @author      nayoayo
  * @date        2021/6/30 8:00 上午
  * @version     1.0
  */
 const path = require('path');
 const fs = require('fs');
+const pluginName = 'copy-modify-webpack-plugin';
 
-/**
- * @param options {[{entry: string, output: string, callback: function }]}
- * @constructor
- */
-function Index(options) {
-	this.options = options
+class CopyModifyWebpackPlugin  {
+
+	/**
+	 * @param options {[{entry: string, output: string, callback: function }]}
+	 * @constructor
+	 */
+	constructor(options) {
+		this.options = options
+	}
+
+	apply(compiler) {
+		let self = this;
+		let folder = compiler.options.context;
+		compiler.hooks.done.tap(pluginName, () => {
+			this.options.forEach((option) => {
+				let entry = path.join(folder, option.entry);
+				let output = path.join(folder, option.output);
+				fs.readFile(entry, 'utf8', function (err, data) {
+					if (option.callback) {
+						data = option.callback(data);
+					}
+					fs.writeFileSync(output, data);
+				});
+			})
+		});
+	}
 }
 
-Index.prototype.apply = function (compiler) {
-	let self = this;
-	let folder = compiler.options.context;
-	compiler.plugin('done', function () {
-		self.options.forEach((option,index) => {
-			let entry = path.join(folder, option.entry);
-			let output = path.join(folder, option.output);
-
-			fs.readFile(entry, 'utf8', function (err, data) {
-				if (option.callback) {
-					data = option.callback(data);
-				}
-				fs.writeFileSync(output, data);
-			});
-		})
-	});
-
-}
-
-module.exports = Index;
+module.exports = CopyModifyWebpackPlugin;
